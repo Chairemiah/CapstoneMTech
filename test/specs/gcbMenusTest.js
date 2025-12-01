f657d812dd76dada207194f46861a55293f7b904
QA Student Afternoon <qastudentafternoon@TT208-04.local>
2025-12-01 13:22:14 -0700

    Add Menus Test to Repo

--- test/specs/gcbMenusTest.js
+++ test/specs/gcbMenusTest.js
@@ -6,26 +6,8 @@ import GCBMenu from '../pageobjects/gcbMenus.js'
 
 describe('GCB Computers Test', () => {
     it('Testing of GCB Website - Dropdown and Menus', async () => {
-        await GCBMenu.open();
-        await GCBMenu.homeBtnHomePage();
-        await GCBMenu.homePageVerify();
-        // await GCBMenu.allWindowsSystemsMenu();
-        // await GCBMenu.linuxOS();
-        // await GCBMenu.allPricingMenu();
-        // await GCBMenu.accessories();
-        // await GCBMenu.repairs();
-        // await GCBMenu.allOurCompanyMenu();
-        // await GCBMenu.open();
-        // await GCBMenu.recycling();
-        // await GCBMenu.gcbLaytonHome();
-        // await GCBMenu.gcbServers();
-        // await GCBMenu.gcbLaytonHome();
-        await GCBMenu.gcbServers();
-        await GCBMenu.gcbServersHomeBtn();
-
-        
-
-
+        await GCBMenu.allGCBMenuItems ();
+        await GCBMenu.allServersMenus();
     })
 })
 
